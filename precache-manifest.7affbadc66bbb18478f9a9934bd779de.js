05baaa770b617d693305f5bbec3a1a007dad945a
d.araya.castillo <d.araya.castillo@accenture.com>
2020-02-27 14:42:41 -0600

    Updates

---
+++ precache-manifest.7affbadc66bbb18478f9a9934bd779de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939544d76a507126f0895c98d6037f19",
+    "url": "/programming-quotes/index.html"
+  },
+  {
+    "revision": "00e94542645d1a6df529",
+    "url": "/programming-quotes/static/css/main.43562104.chunk.css"
+  },
+  {
+    "revision": "bad0fd921756cb5aeae7",
+    "url": "/programming-quotes/static/js/2.75d60ee0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/programming-quotes/static/js/2.75d60ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e94542645d1a6df529",
+    "url": "/programming-quotes/static/js/main.58e88d58.chunk.js"
+  },
+  {
+    "revision": "5788fcc2d011aa7840fb",
+    "url": "/programming-quotes/static/js/runtime-main.51a956a3.js"
+  },
+  {
+    "revision": "34bccda576e79ff6bd601d83b6a239da",
+    "url": "/programming-quotes/static/media/hero.34bccda5.svg"
+  }
+]);
